5dc754315d3e91293bead3556f69b27757119aef
CHAT41ECL <81322130+CHAT41ECL@users.noreply.github.com>
2021-03-24 12:37:35 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDdapj3H3Dd2w9DHpLHwbzwpwSwU7rItpg",
-    authDomain: "chatrickfb.firebaseapp.com",
-    projectId: "chatrickfb",
-    storageBucket: "chatrickfb.appspot.com",
-    messagingSenderId: "815561540833",
-    appId: "1:815561540833:web:2cf0f52c625e8e281723a0",
-    measurementId: "G-5W02XQR53J"
+    apiKey: "AIzaSyCLntOIQOEr3DSu_MLuDP8qxVAsQLKozGU",
+    authDomain: "chat41ecl.firebaseapp.com",
+    projectId: "chat41ecl",
+    storageBucket: "chat41ecl.appspot.com",
+    messagingSenderId: "473275587460",
+    appId: "1:473275587460:web:385b5a1c726152e0341bd1",
+    measurementId: "G-D4PEE5DB3B"
     });
